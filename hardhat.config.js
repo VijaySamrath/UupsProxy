3fbfda30bceb02d4565c688125b8c998de21392c
vijay samrath <vijaysamrath462@gmail.com>
2023-07-22 22:25:30 +0530

    update commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,15 +3,16 @@ require("@nomiclabs/hardhat-etherscan");
 require('@openzeppelin/hardhat-upgrades');
 require("dotenv").config();
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.18",
   networks: {
-    goerli: {
-      url: process.env.GOERLI_URL,
-      accounts: [process.env.GOERLI_PRIVATE_KEY]
-    }
+    sepolia: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_KEY
-  }
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
